7fcd4891c2c12cad165bad005b7c91c31c279d7a
Vitor Frota <vitorfrota@live.com>
2020-05-22 13:42:37 -0400

    Aula 18 - Servindo arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import express from 'express';
+import uploadConfig from './config/upload';
 
 import routes from './routes';
 import './database';
 
 const app = express();
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes); // middleware routes
 
 app.listen(3333, () => {
