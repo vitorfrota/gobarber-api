208c7e5a6487aa2bbcc766b9a0e24b78048720ec
Vitor Frota <vitorfrota@live.com>
2020-05-22 00:23:52 -0400

    Aula 11 - Criptografia de senha

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,39 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns'; // parseISO é de string para date
+import { getCustomRepository } from 'typeorm';
+
+import AppointmentsRepository from '../repositories/AppointmentRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  try {
+    const appointments = await appointmentsRepository.find();
+
+    return response.json(appointments);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+appointmentsRouter.post('/', async (request, response) => {
+  const { provider_id, date } = request.body;
+  try {
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider_id,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
