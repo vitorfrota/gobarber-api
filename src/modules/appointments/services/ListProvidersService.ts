35591e2c1c4a88a2329268adceaf112f51878b51
Vitor Frota <vitorfrota@live.com>
2020-07-22 09:35:24 -0400

    correção serialização providers

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
@@ -25,7 +26,7 @@ export default class ListProvidersService {
       users = await this.usersRepository.findAllProviders({
         except_user_id: user_id
       });
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(`providers-list:${user_id}`, classToClass(users));
     }
 
     return users;
